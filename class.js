a533205314f918228c3b07077f24372a7684f8ac
Ester <ester_c_rocha@estudante.sesisenai.org.br>
2024-03-14 09:00:44 -0300

    Class

---
+++ class.js
@@ -0,0 +1,31 @@
+class Obj{
+
+    des_obj(){
+
+    }
+}
+class Aviao{
+
+}
+
+class Aviao extends Obj{
+    des_aviao(){
+
+    }
+
+    atual_aviao(){
+    }
+}
+
+class Nuvem extends Obj{
+    atual_nuvem(){
+
+        }
+        recomeca(){
+
+        }
+    } 
+
+class Text{
+
+}
