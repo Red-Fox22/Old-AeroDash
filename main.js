6d75265a428e34af16d3457165830264d203e5ae
Miguel <miguel_zacca@estudante.sesisenai.org.br>
2024-03-18 09:54:33 -0300

    miguel

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-const canvas = document.querySelector('canvas');
-const ctx = canvas.getContext('2d');
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
 
 canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight;
@@ -8,10 +8,10 @@ const centerX_canvas = canvas.width / 2;
 const centerY_canvas = canvas.height / 2;
 
 const background = new Image();
-background.src = './assets/background.webp';
+background.src = "./assets/background.webp";
 
 const plane = new Image();
-plane.src = './assets/plane.webp';
+plane.src = "./assets/plane.webp";
 
 const back = new Back(background, 10);
 
@@ -19,6 +19,4 @@ const gameLoop = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   back.draw();
   requestAnimationFrame(gameLoop);
-}
-
-gameLoop();
+};
