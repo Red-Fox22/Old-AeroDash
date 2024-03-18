1d1300c56b3259f245f320499ae66dfab1e76194
Ester <ester_c_rocha@estudante.sesisenai.org.br>
2024-03-18 08:14:41 -0300

    main editado

--- main.js
+++ main.js
@@ -1,1 +1,38 @@
-// let my_plane = 
+let des = document.getElementById('des').getContext('2d')
+
+let aviao = new Aviao
+let aviao1  = new Avioes
+let aviao2  = new Avioes
+
+let t1 = new Text()
+let t2 = new Text()
+
+document.addEventListener('keydown',(e)=>{
+    // console.log(e.key)
+    if(e.key === 'a'){
+        aviao.dir -= 40
+    }else if(e.key === 'd'){
+        aviao.dir += 40
+    }
+})
+document.addEventListener('keyup', (e)=>{
+    if(e.key === 'a'){
+        aviao.dir = 0
+    }else if(e.key === 'd'){
+        aviao.dir = 0
+    }
+})
+
+function desenha(){
+
+}
+function atualiza(){
+
+}
+function main(){
+    des.clearRect(0,0,500,700)
+    desenha()
+    atualiza()
+}
+
+setInterval(main,10)
